69762dafc49383562c8eb7490f9f4ac96dec8fb3
Iqra <attari1235@gmail.com>
2022-01-08 15:44:05 +0500

    login added

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
   collection,
   deleteDoc,
 } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -29,26 +29,114 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+let edit = false
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth();
+
 const analytics = getAnalytics(app);
 const listContainer = document.getElementById("list_container");
+
 const db = getFirestore();
+
 const studentForm = document.getElementById("add");
+
 const registerForm = document.getElementById("register");
+const loginForm = document.getElementById("login");
+
 const studentsColRef = collection(db, "student");
 
-studentForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+// studentForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   if (!edit) {
+//     console.log('Add wla chala')
+//     addDoc(studentsColRef, {
+//       name: studentForm.name.value,
+//       marks: studentForm.marks.value,
+//       class: studentForm.class.value,
+//     }).then(() => {
+//       studentForm.reset();
+//     });
+//   }
 
-  addDoc(studentsColRef, {
-    name: studentForm.name.value,
-    marks: studentForm.marks.value,
-    class: studentForm.class.value,
-  }).then(() => {
-    studentForm.reset();
-  });
-});
+// });
+
+
+
+// const addListItem = (obj) => {
+//   const li = document.createElement("li");
+//   const txt = document.createElement("span");
+//   txt.append(obj.name);
+//   const btn = document.createElement("button");
+//   const editBtn = document.createElement("button");
+//   btn.append("Delete");
+//   editBtn.append('Edit')
+//   btn.setAttribute("id", obj.id);
+//   editBtn.setAttribute("id", obj.id);
+//   li.append(txt);
+//   li.append(btn);
+//   li.append(editBtn);
+//   listContainer.appendChild(li);
+
+//   btn.addEventListener("click", (e) => deleteDocument(e.target.id));
+
+//   editBtn.addEventListener("click", (e) => editDocument(e.target.id));
+// };
+
+// const unsubscribe = onSnapshot(
+//   studentsColRef,
+//   (snapshot) => {
+//     let students = [];
+//     snapshot.forEach((doc) => {
+//       students.push({ id: doc.id, ...doc.data() });
+//     });
+//     console.log('students==>', students)
+//     showTheStudents(students);
+//   },
+//   (error) => {
+//     console.log("No such document!");
+//   }
+// );
+
+// const showTheStudents = (arr) => {
+//   listContainer.innerHTML = null;
+//   arr.map((data) => addListItem(data));
+// };
+
+// const deleteDocument = async (id) => {
+//   const deleted = await deleteDoc(doc(db, "student", id));
+//   console.log("deleted=>", deleted);
+// };
+
+// const editDocument = async (id) => {
+//   console.log('id=>', id)
+//   edit = true
+//   let docRef = doc(db, 'student', id)
+//   let studentObj = await getDoc(docRef)
+//   studentObj = studentObj.data()
+//   studentForm.name.value = studentObj.name
+//   studentForm.marks.value = studentObj.marks
+//   studentForm.class.value = studentObj.class
+
+//   const btn = studentForm.getElementsByTagName('button')
+//   console.log(btn)
+//   btn[0].innerText = 'Update Student'
+//   btn[0].addEventListener('click', () => {
+//     if (edit) {
+//       setDoc(docRef, {
+//         name: studentForm.name.value,
+//         marks: studentForm.marks.value,
+//         class: studentForm.class.value,
+//       }).then(() => {
+//         studentForm.reset();
+//       });
+//     }
+
+//   })
+//   btn[0].innerText = 'Add Student'
+//   edit = false
+// }
+
 
 registerForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -61,53 +149,35 @@ const registerUser = async (email, password) => {
       // Signed in
       const user = userCredential.user;
       console.log(user);
+      registerForm.reset()
       // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
+      alert(error.message)
       // ..
     });
 };
 
-const addListItem = (obj) => {
-  const li = document.createElement("li");
-  const txt = document.createElement("span");
-  txt.append(obj.name);
-  const btn = document.createElement("button");
-  btn.append("Delete");
-  btn.setAttribute("id", obj.id);
-  li.append(txt);
-  li.append(btn);
-  listContainer.appendChild(li);
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  loginUser(loginForm.email.value, loginForm.password.value);
+});
 
-  btn.addEventListener("click", (e) => deleteDocument(e.target.id));
-};
-
-const unsubscribe = onSnapshot(
-  studentsColRef,
-  (snapshot) => {
-    let students = [];
-    snapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      students.push({ id: doc.id, ...doc.data() });
-      // addListItem({ id: doc.id, ...doc.data() });
+const loginUser = async (email, password) => {
+  await signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+      alert(user.email)
+      loginForm.reset()
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(error.message)
+      // ..
     });
-
-    showTheStudents(students);
-  },
-  (error) => {
-    console.log("No such document!");
-  }
-);
-
-const showTheStudents = (arr) => {
-  listContainer.innerHTML = null;
-  arr.map((data) => addListItem(data));
-};
-
-const deleteDocument = async (id) => {
-  console.log("id=>", id);
-  const deleted = await deleteDoc(doc(db, "student", id));
-  console.log("deleted=>", deleted);
 };
